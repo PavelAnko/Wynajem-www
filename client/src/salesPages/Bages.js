32b7af58cebb09ff6d5ba5ae51e4399c212c8a51
PavelAnko <pavelanwork@gmail.com>
2022-07-05 13:03:07 +0300

    finish

--- client/src/salesPages/Bages.js
+++ client/src/salesPages/Bages.js
@@ -1,52 +1,6 @@
 import React from 'react';
 import '../style/bages.css';
 
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
 const Bages = () => {
   return (
     <div class='bages'>
@@ -65,9 +19,22 @@ const Bages = () => {
         </div>
         <div class="title-2">
           <div class="s-a-f-c">
-            <div class="shop">
-              SHOP
-            </div>
+            <div class="shop"> SHOP
+              <div class="popups">
+                <div class="bufer-pop"></div>   
+                <div class="block">
+                  <a href="6" class="href"><div class="block-text">
+                    BACKPACKS  
+                  </div></a>
+                  <a href="6" class="href"><div class="block-text">
+                    DUFFLE BAGS
+                  </div></a>
+                  <a href="6" class="href"><div class="block-text">
+                    TRAVEL PACKS
+                  </div></a>
+                </div>
+              </div>                          
+            </div> 
             <div class="about">
               ABOUT
             </div>
@@ -78,10 +45,14 @@ const Bages = () => {
               CONTACT
             </div>          
           </div>
-          <div class="search">  
-            <SearchIcon />                              
+          <div class="search"> 
+            <input class="text" type="text" placeholder="SEARCH..."></input> 
+            <a href="3"><div class="png-transp"></div></a>                                        
           </div>
-          <div class="log-in"></div>
+          <div class="log-in">
+            <a href="3"><div class="log-icon"></div></a>
+            <a class="href" href="#"><div class="log">Log In</div></a>
+          </div>
           <div class="basket"></div>
         </div>
       </div> 
@@ -117,14 +88,96 @@ const Bages = () => {
             <div class="href-img">BUFFLE BAGS</div>
           </a>
         </div>
-        <div class="travel-packs"> 
+        <div class="travel-packs">
           <a class="href" href="3">
             <div class="href-img">TRAVEL PACKS</div>             
           </a>  
         </div> 
       </div>
       <div class="bags-3">
-      </div>       
+        <div class="now-on-s">NOW ON SALE</div>
+        <div class="up-to-50">UP TO 50% OFF</div>
+        <div class="choice-bags">
+          <div class="block-bags">
+            <div class="bag-1"></div>
+            <div class="name-bag">CANVAS BACKPACK 
+              <div class="display">
+                <div class="line-through">$75.00</div>$67.50
+              </div>              
+            </div>
+            <a href="2" class="href">
+              <div class="hover-name-bag">View Details</div>
+            </a>
+          </div>
+          <div class="block-bags">
+            <div class="bag-2"></div>
+            <div class="name-bag">TECHNICAL PACK 
+              <div class="display">
+                <div class="line-through">$130.00</div>$104.00
+              </div>              
+            </div>
+            <a href="2" class="href">
+              <div class="hover-name-bag">View Details</div>
+            </a>
+            <div class=""></div>
+          </div>
+          <div class="block-bags">
+            <div class="bag-3"></div>
+            <div class="name-bag">CAMO BACKPACK 
+              <div class="display">
+                <div class="line-through">$95.00</div>$76.00
+              </div>              
+            </div>
+            <a href="2" class="href">
+              <div class="hover-name-bag">View Details</div>
+            </a>
+          </div>
+        </div>
+      </div> 
+        <div class="desert">
+          <div class="block-desert">
+            <div class="desert-text">
+              <div class="desert-title">ABOUT US</div>
+              <div class="desert-info">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</div>
+            </div>
+          </div>
+        </div>
+        <div class="block-contant">
+          <div class="contact-blok-1">
+            <div class="title-contant-1">First Name * 
+              <div class="title-contant-2">Last Name *</div>
+            </div>
+            <input class="text-contant-1 hover-text" type="text"></input>
+            <input class="text-contant-2 hover-text" type="email"></input>
+            <div class="title-contant-1">Enter Your Email *</div>
+            <input class="text-contant-3 hover-text" type="text"></input>
+            <div class="title-contant-1">Enter Your Subject</div>
+            <input class="text-contant-3 hover-text" type="text"></input>
+            <div class="title-contant-1 ">Enter Your Message</div>
+            <input class="text-contant-4 hover-text" type="text"></input> 
+            <a href="2" class="href">
+              <button class="button-contant" type="button">Send</button>
+            </a>        
+          </div>
+          <div class="contact-blok-2">
+            <div class="title-contact-blok-2">CONTACT</div>
+            <div class="info-contact-blok-2">I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.</div>
+            <div class="tel-contact-blok-2">info@my-domain.com <br></br>Tel: 1-800-000-0000</div>
+          </div>
+        </div>
+        <div class="join-list">
+          <div class="block-join-list">
+            <div class="title-join-list">JOIN OUR MAILING LIST</div>
+            <div class="subtitle-join-list">AND NEVER MISS AN UPDATE</div>
+            <div class="enter-join-list">Enter your email here *</div>
+            <a href="2" class="href">
+              <button class="button-join-list-1" type="button">EMAIL</button>
+            </a>  
+            <a href="2" class="href">
+              <button class="button-join-list-2" type="button">SUBSCRIBE NOW</button>
+            </a>  
+          </div>
+        </div>     
     </div>
   );
 }
